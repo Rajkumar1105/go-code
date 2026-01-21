011798798bbff42030a620d453b168f6f8c31b5d
shubham_dixit <shubam.dixit@janio.asia>
2026-01-21 16:15:25 +0530

    Interfaces

--- main.go
+++ main.go
@@ -2,43 +2,86 @@ package main
 
 import "fmt"
 
-type Data struct {
+// interfaces --have some kind of contract or some kind of agreement with the objects
+type Humans struct {
 	Name string
 }
 
-//func (d *Data) GetName() string {
-//	return d.Name
-//}
-
-func (v *Data) SetName(name string) {
-	v.Name = name
+func (h Humans) Angry() {
+	fmt.Println(h.Name, "is angry")
 }
 
-// lets say db connectioio   App ---->Db connection
+func (h Humans) Speak() {
+	fmt.Println(h.Name, "is speaking in telugus")
+}
 
+func (h Humans) Hunt() {
+	fmt.Println(h.Name, "hunting mosquitos")
+}
+func (h Humans) Play() {
+	fmt.Println(h.Name, "is playing crickte")
+}
+func (h Humans) Eat() {
+	fmt.Println(h.Name, "is easting hyderbadi birayni")
+}
+
+type Cat struct {
+	Tail int
+}
+
+func (h Cat) Angry() {
+	fmt.Println("is angry")
+}
+
+func (h Cat) Speak() {
+	fmt.Println("is roaring")
+}
+
+func (h Cat) Hunt() {
+	fmt.Println("cat would be eayting rabbits or deers")
+}
+func (h Cat) Play() {
+	fmt.Println("Playing  with tigers")
+}
+func (h Cat) Eat() {
+	fmt.Println("will eat dear")
+}
+
+// the whole purpose of an interface is to give the generallized behaviour for concrete objects
+type Animal interface {
+	Angry()
+	Speak()
+	Eat()
+	Play()
+	Hunt()
+	Crawl(p int) string
+}
+
+// interface{}
+func Party(p interface{}) {
+	fmt.Println(p)
+}
+
+// interface{} is a super type and every object and every built in type satisfies it
 func main() {
-	//c := Data{
-	//	Name: "shubham",
+
+	mahesh := Humans{
+		Name: "Mahesh",
+	}
+
+	mahesh.Eat()
+	mahesh.Angry()
+	mahesh.Hunt()
+	mahesh.Speak()
+	mahesh.Play()
+
+	// We can say mahesh is animal
+
+	//tiger := Cat{
+	//	Tail: 1,
 	//}
 
-	// new keyword creates the object and returns pointer
-
-	c := new(Data)
-
-	c.SetName("Shubham")
-
-	fmt.Println(c)
-
-	// new and make
-	//c.SetName("John")
-	//fmt.Println(c)
-
-	//var f *Data
-	//
-	//f = &c
-	//
-	//fmt.Println(f) // f is a pointer to struct
-	//
-	//fmt.Println((*f).Name) // so first dereference the struct  then we are accessing the values
-
+	Party(3)
+	Party("ssay")
+	Party(mahesh)
 }
